ade7be1d532ada0ba7c4b795019e5398f24b3f20
vicky-vkhub <52245079+vicky-vkhub@users.noreply.github.com>
2021-05-12 13:23:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,49 +5,30 @@ import smtplib, requests, time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 #from lxml import html
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+#chrome_options = webdriver.ChromeOptions()
+#chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+#chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--disable-dev-shm-usage")
+#chrome_options.add_argument("--no-sandbox")
+#driver = webdriver.Chrome(executable_path=os.environ.get("/app/.chromedriver/bin/chromedriver"), chrome_options=chrome_options)
 
 #exec_path = r'C:\Users\muruk\Downloads\geckodriver-v0.29.1-win64\geckodriver.exe'
+exec_path = r'C:\Users\Mumenrider\Downloads\chromedriver_win32\chromedriver.exe'
 URL = 'https://www.cowin.gov.in/home'
 input_locator = 'mat-input-0'
 search_Button = '/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[2]/div/div/button'
-Search_text = ['600032', '600089', '600125', '600087', '600011', '600037', '600040', '600127', '600095']
+Search_text = ['600032', '600089', '600125', '600087', '600037', '600040', '600127', '600006', '600095', '620007', '600075', '600044', '600043', '400053', '626003']
 #, '600089', '600125', '600087', '600011', '600037', '600040', '600127'
 Age_18_locator = '/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[4]/div/div[1]/label'
 
 
-email_recipients = ['vigneshkrish.vk@gmail.com', 'ironmanvkspam@gmail.com', 'abilashcod@gmail.com', 'aragooner6@gmail.com', 'nikhil.mahend@gmail.com']
-# creates SMTP session
-s = smtplib.SMTP("smtp.gmail.com", 587)
+email_recipients = ['ironmanvkspam@gmail.com', 'abilashmadhan@gmail.com', 'aragooner6@gmail.com', 'thamizhnambi2gmail.com']
 
-# start TLS for security
-s.starttls()
-
-# Authentication
-s.login("cowin.vaccineupdate@gmail.com", "gocoronago")
-
-# Instance of MIMEMultipart
-msg = MIMEMultipart("alternative")
-
-# Write the subject
-msg["Subject"]= "Vaccine Available!"
-
-#msg["From"]="sender_gmail_id"
-#msg["To"]="receiver_gmail_id"
-
-# Plain text body of the mail
-text = " Vacacine is Available Now!"
-
-# Attach the Plain body with the msg instance
-msg.attach(MIMEText(text, "plain"))
 
 
 #driver = webdriver.Firefox(executable_path=exec_path)
+driver = webdriver.Chrome(executable_path=exec_path)
+
 driver.get(URL)
 driver.maximize_window()
 while(1):
@@ -89,6 +70,33 @@ while(1):
                     print(Area)
                     print(Age_Category)
                     print(Hospital)
+
+                    #print(msg.as_string())
+                    # Sending the mail
+                    # creates SMTP session
+                    s = smtplib.SMTP("smtp.gmail.com", 587)
+
+                    # start TLS for security
+                    s.starttls()
+                    s.ehlo()
+
+                    # Authentication
+                    s.login("cowin.vaccineupdate@gmail.com", "gocoronago")
+
+                    # Instance of MIMEMultipart
+                    msg = MIMEMultipart("alternative")
+
+                    # Write the subject
+                    msg["Subject"] = "Vaccine Available!"
+
+                    # msg["From"]="sender_gmail_id"
+                    # msg["To"]="receiver_gmail_id"
+
+                    # Plain text body of the mail
+                    text = " Vacacine is Available Now!"
+
+                    # Attach the Plain body with the msg instance
+                    msg.attach(MIMEText(text, "plain"))
                     # HTML body of the mail
                     emailtext = ' Manufacturer: ' + Vacc_name + ' At ' + Hospital + ' ' + 'for Category ' + Age_Category
                     print(emailtext)
@@ -97,8 +105,18 @@ while(1):
                     # Attach the HTML body with the msg instance
                     msg.attach(MIMEText(html, "html"))
 
-                    #print(msg.as_string())
-                    # Sending the mail
-                    s.sendmail("ironmanvkspam@gmail.com", email_recipients, msg.as_string())
+                    if i == 8:
+                        s.sendmail("cowin.vaccineupdate@gmail.com", 'nikhil.mahend@gmail.com', msg.as_string())
+                        s.sendmail("cowin.vaccineupdate@gmail.com", email_recipients, msg.as_string())
+                    elif i == 9:
+                        s.sendmail("cowin.vaccineupdate@gmail.com", 'rahul090597@gmail.com', msg.as_string())
+                    elif (i == 10) or (i == 11) or (i == 12):
+                        s.sendmail("cowin.vaccineupdate@gmail.com", 'aakash.raj5@gmail.com', msg.as_string())
+                    elif i == 13:
+                        s.sendmail("cowin.vaccineupdate@gmail.com", 'microice15@gmail.com', msg.as_string())
+                    elif i == 14:
+                        s.sendmail("cowin.vaccineupdate@gmail.com", 'rlmethun@gmail.com', msg.as_string())
+                    else:
+                        s.sendmail("cowin.vaccineupdate@gmail.com", email_recipients, msg.as_string())
                     #s.quit()
                     print('sent')
